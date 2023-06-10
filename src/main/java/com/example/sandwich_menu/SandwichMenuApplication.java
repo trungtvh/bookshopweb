4f0ba97775804f8a136e132af44b807be0c23f94
huutrungu <huutrungg02@gmail.com>
2023-06-10 08:52:44 +0700

    first commit

---
+++ src/main/java/com/example/sandwich_menu/SandwichMenuApplication.java
@@ -0,0 +1,13 @@
+package com.example.sandwich_menu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SandwichMenuApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SandwichMenuApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/sandwich_menu/SandwichMenuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sandwich_menu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SandwichMenuApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
